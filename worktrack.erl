20695a1544e6b73a9027e93ff8fd88c4eb0e6144
Ytrog <hisbrucker@gmail.com>
2013-09-29 22:00:34 +0200

    rudimentary database working (one ets table)

--- worktrack.erl
+++ worktrack.erl
@@ -1,18 +1,61 @@
 -module(worktrack).
--export([start/0]).
+%-export([start/0]).
+-compile(export_all).
 -include("worktrack.hrl").
+
+start() ->
+    %% starts the program
+    io:format("Starting worktrack ~n"),
+    init(). % will be spawn
+
+init() ->
+    %% init the process
+    ets:new(work, [bag, named_table]),
+    main().
+    
+main() ->
+    %% main loop
+    Action = prompt("Task (type help for help) > "),
+    dispatch(Action),
+    main().
+
+dispatch("add") ->
+    add();
+dispatch("help") ->
+    help();
+dispatch("all") ->
+    display_all();    
+dispatch("exit") ->
+    exit();
+dispatch(Action) ->
+    io:format("Unknown action ~p ~n", [Action]).
+
+%% private
 
 add() ->
     %% ask for each field
-    ItemName = prompt("Name of the item: "),
-    Date = prompt("Date (dd-MM-yyyy): "),
-    WorkTime = prompt("Time worked on: "),
+    ItemName = prompt("Name of the item> "),
+    Date = prompt("Date (dd-MM-yyyy)> "),
+    WorkTime = prompt("Time worked on> "),
     Item = {item, ItemName, Date, WorkTime},
     add(Item).
     
 add(Item) ->
     %% add to storage (dict, file, ets, mnesia ... something like that)
+    ets:insert(work, Item).
+    
+display_all() -> % don't want to display all eventualy
+    Work = ets:tab2list(work),
+    lists:foreach(fun(W) -> io:format("~p ~n", [W]) end, Work).
+
+help() ->
     throw(notimplemented).
+    
+exit() ->
+    io:format("Exiting"),
+    exit(normal).
+    
+%%% Utils
     
 prompt(Prompt) ->
     %% displays prompt and let people type in an answer
